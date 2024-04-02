c2f5bd0bf49053536e724cee237f9efe16279fef
Abdulghani Almasri <a-almasri@outlook.com>
2024-04-02 16:05:53 +0300

    Updating the session class

--- components/session.py
+++ components/session.py
@@ -1,42 +1,190 @@
 from typing import List, Union
 import numpy as np
-import torch
-from bert_score import BERTScorer
-import seaborn as sns
-import matplotlib.pylab as plt
+from utils import compute_statistic
+from torch import nn
 import pandas as pd
 
 
 class QuerySession:
-    """
-    """
-    def __init__(self, queries: List[str] = None, normalize_embeddings: bool = False, embedding_model: str = None) -> None:
+    """ """
+
+    def __init__(
+        self,
+        queries: List[str] = None,
+        normalize_embeddings: bool = False,
+        embedding_model: nn.Sequential = None,
+        context_window_size: int = 1,
+        statistic: str = "mean",
+    ) -> None:
         """
+        Initialize the QuerySession object
+            Args:
+                queries (List[str]): List of queries
+                normalize_embeddings (bool): Use normalized embeddings
+                embedding_model (str): Sentence embedding model
+                context_window_size (int): Context window size
+                statistic (str): Statistic to use when computing global coherence score
+
+            Returns:
+                None
         """
         if queries is not None:
-            self.queries: List[str] = queries[:]
-            self._embed_queries(queries=queries, embedding_model=embedding_model, normalzied=normalize_embeddings)
-               
+            self.queries = queries[:]
+            self.session_size = len(queries)
+            self.embedding_model = embedding_model
+            self.normalized_session = normalize_embeddings
+            self.embeddings = self._embed_queries(
+                queries=queries,
+                embedding_model=self.embedding_model,
+                normalize_embeddings=self.normalized_session,
+            )
+            self.global_coherence_score, self.local_coherence_scores = (
+                self.compute_global_coherence(
+                    context_window=context_window_size, statistic=statistic
+                )
+            )
+            self.statistic = statistic
+            self.context_window_size = context_window_size
 
-        self.coherence_score = 0.0
+    def _embed_queries(
+        self,
+        queries: List[str],
+        embedding_model: str = None,
+        normalize_embeddings: bool = False,
+    ) -> np.ndarray:
+        """
+        Embed the list of queries
+            Args:
+                queries (List[str]): List of queries as string
+                embedding_model (str): Sentence embedding model
+                normalize_embeddings (bool): Use normalized embeddings
 
-    def add_query(self, query: str) -> None:
-        """
-        """
-        self.queries.append(query)
-
-    def _embed_queries(self, queries: List[str], embedding_model: str = None, normalzied = False) -> None:
-        """
+            Returns:
+                np.ndarray: Embedded queries
         """
         if embedding_model is not None:
-            self.embeddings = embedding_model.embed(sentences=queries, normalzied=normalzied)[:]
+            return embedding_model.embed(
+                sentences=queries, normalize_embeddings=normalize_embeddings
+            )
 
-    def _compute_coh_score(self, embedding_model: str = None) -> None:
+    def _add_query(self, query: str = None) -> None:
         """
+        Add a query to the session
+            Args:
+                query (str): Query to add to the session as a string
+
+            Returns:
+                None
         """
-        if embedding_model is not None:
-            model = EmbeddingModel(model_name=embedding_model)
-            self.embeddings = model.embed(sentences=self.queries).detach().numpy()
-            self.coh_score = np.corrcoef(self.embeddings)[0, 1]
+        if query is not None:
+            self.queries.append(query)
+            self.session_size += 1
+            query_embedding = self.embedding_model.embed(
+                sentences=[query], normalize_embeddings=self.normalized_session
+            )
+            self.embeddings = np.concatenate([self.embeddings, query_embedding])
+            context_vector = self._compute_context_vector(
+                position=self.session_size - 1, context_window=self.context_window_size
+            )
+            local_coherence_score = self._compute_local_coherence(
+                current_vector=query_embedding[0], neighbor_vector=context_vector
+            )
 
+            self.local_coherence_scores.append(local_coherence_score)
 
+            self.global_coherence_score = compute_statistic(
+                numbers=self.local_coherence_scores, statistic=self.statistic
+            )
+
+    def add_queries(self, queries: Union[str, List[str]]) -> None:
+        """
+        Add queries to the session
+            Args:
+                queries (Union[str, List[str]]): Query or list of queries to add to the session
+
+            Returns:
+                None
+        """
+        if isinstance(queries, str):
+            self._add_query(query=queries)
+        elif isinstance(queries, list):
+            for query in queries:
+                self._add_query(query=query)
+        else:
+            raise ValueError("Invalid input type. Expected str or list of str.")
+
+    def _compute_context_vector(
+        self, position: int = 1, context_window: int = 1
+    ) -> np.ndarray:
+        """
+        Compute the context vector for a given query in the session
+            Args:
+                position (int): Position of the query in the session
+                context_window (int): Context window size
+            Returns:
+                np.ndarray: Context vector as a numpy array
+        """
+        if context_window >= self.session_size:
+            raise ValueError("Context window is too big")
+
+        if position < context_window:
+            context_vector = np.sum(self.embeddings[:position], axis=0)
+            context_vector = context_vector / np.linalg.norm(context_vector)
+
+        else:
+            context_vector = np.sum(
+                self.embeddings[position - context_window : position], axis=0
+            )
+            context_vector = context_vector / np.linalg.norm(context_vector)
+
+        return context_vector
+
+    def _compute_local_coherence(
+        self, current_vector: np.ndarray = None, neighbor_vector: np.ndarray = None
+    ) -> float:
+        """
+        Compute the local coherence between a query and its context vector
+            Args:
+                current_vector (np.ndarray): Current query embedding
+                neighbor_vector (np.ndarray): Context vector
+
+            Returns:
+                float: Local coherence score
+        """
+        if current_vector is not None and neighbor_vector is not None:
+            return np.dot(current_vector, neighbor_vector) / (
+                np.linalg.norm(current_vector) * np.linalg.norm(neighbor_vector)
+            )
+
+    def compute_global_coherence(
+        self, context_window: int = 1, statistic: str = "mean"
+    ) -> float:
+        """
+        Compute the global coherence score for the session
+            Args:
+                context_window (int): Context window size
+                statistic (str): Statistic to use when computing the global coherence score
+
+            Returns:
+                float: Global coherence score
+        """
+        if self.session_size > 1:
+
+            local_coherence_scores = []
+
+            for i, embedding in enumerate(self.embeddings):
+                if i == 0:
+                    continue
+                context_vector = self._compute_context_vector(
+                    position=i, context_window=context_window
+                )
+                local_coherence_scores.append(
+                    self._compute_local_coherence(
+                        current_vector=embedding, neighbor_vector=context_vector
+                    )
+                )
+
+        return (
+            compute_statistic(numbers=local_coherence_scores, statistic=statistic),
+            local_coherence_scores,
+        )
--- components/utils.py
+++ components/utils.py
@@ -1,7 +1,9 @@
+import statistics
+from typing import List
+import torch
 
-import torch 
 
-#Mean Pooling - Take attention mask into account for correct averaging
+# Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask) -> torch.Tensor:
     """
     Mean pooling to get sentence embeddings
@@ -13,8 +15,35 @@ def mean_pooling(model_output, attention_mask) -> torch.Tensor:
         Returns:
             torch.Tensor: Sentence embeddings
     """
-    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    token_embeddings = model_output[
+        0
+    ]  # First element of model_output contains all token embeddings
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(
+        input_mask_expanded.sum(1), min=1e-9
+    )
 
 
+# Compute the mean, median, or mode of a list of numbers
+def compute_statistic(numbers: List[float], statistic: str = "mean") -> float:
+    """
+    Calculate the mean, median, or mode of a list of numbers
+        Args:
+            numbers (List): List of numbers
+            statistic (str): Statistic to calculate
+
+        Returns:
+            float: Calculated statistic
+    """
+    if statistic == "mean":
+        return statistics.mean(numbers)
+    elif statistic == "median":
+        return statistics.median(numbers)
+    elif statistic == "mode":
+        return statistics.mode(numbers)
+    else:
+        raise ValueError(
+            "Invalid statistic parameter. Expected 'mean', 'median', or 'mode'."
+        )
